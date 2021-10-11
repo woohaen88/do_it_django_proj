fde8e0edf911bdd49ecb570bcfe2a78b2064e0eb
woohaen88 <woohaen88@gmail.com>
2021-10-11 12:43:21 +0000

    태그 페이지 만들기

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls.conf import path
-from .views import PostListView, PostDetailView, category_page
+from .views import PostListView, PostDetailView, category_page, tag_page
 
 
 app_name = 'blogapp'
 
 urlpatterns = [
-    path('category/<str:slug>/', PostListView.as_view(), name='list'),
+    path('tag/<str:slug>/', tag_page, name='tag'),
+    path('category/<str:slug>/', category_page, name='category'),
     path('list/', PostListView.as_view(), name='list'),
     path('<int:pk>/', PostDetailView.as_view(), name='detail'),
 ]
--- blogapp/views.py
+++ blogapp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import ListView, DetailView
-from .models import Post, Category
+from .models import Post, Category, Tag
 
 class PostListView(ListView):
     model = Post
@@ -32,11 +32,26 @@ def category_page(request, slug):
 
     return render(
         request,
-        'blog/post_list.html',
+        'blogapp/list.html',
         {
             'post_list': post_list,
             'categories': Category.objects.all(),
             'no_category_post_count': Post.objects.filter(category=None).count(),
             'category': category,
+        }
+    )
+
+def tag_page(request, slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
+
+    return render(
+        request,
+        'blogapp/list.html',
+        {
+            'post_list': post_list,
+            'tag': tag,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
         }
     )
