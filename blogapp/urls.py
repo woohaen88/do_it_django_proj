aca011f3611e508248ef7d4ed0b78ff975c78827
woohaen88 <woohaen88@gmail.com>
2021-10-11 15:37:55 +0000

    댓글 삭제 기능 추가하기

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls.conf import include, path
-from .views import PostListView, PostDetailView, category_page, tag_page, PostCreate, PostUpdate, new_comment, CommentUpdate
+from .views import PostListView, PostDetailView, category_page, tag_page, PostCreate, PostUpdate, new_comment, CommentUpdate, delete_comment
 
 
 app_name = 'blogapp'
 
 urlpatterns = [
+    path('delete_comment/<int:pk>/', delete_comment, name='delete'),
     path('update_post/<int:pk>/', PostUpdate.as_view(), name='update'),
     path('create_post/', PostCreate.as_view(), name='create'),
     path('tag/<str:slug>/', tag_page, name='tag'),
