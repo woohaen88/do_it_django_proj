6a1f15f63e03d5df2674ba88a45729935100f730
woohaen88 <woohaen88@gmail.com>
2021-10-11 07:52:35 +0000

    포스트 목록, 상세 페이지에서 대표 이미지 없을 때 문제 해결하기

--- blogapp/models.py
+++ blogapp/models.py
@@ -7,9 +7,12 @@ class Post(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    head_image = models.ImageField(upload_to='blog/images/%y/%m/%d/', blank=True)
+    file_upload = models.FileField(upload_to='blog/files/%y/%m/%d/', blank=True)
+    
 
     def __str__(self):
         return '[{}] {}'.format(self.pk, self.title)
 
     # def get_absolute_url(self):
-    #     return f'/blog/{self.pk}/'
+    #     return f'/blog/{self.pk}/' 
